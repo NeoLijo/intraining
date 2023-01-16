321402e5d411540225d2f4815ce676572d155244
USER <USER@HP.bbrouter>
2023-01-16 08:06:39 -0800

    first commit

---
+++ ObsquraTraining/src/in/training/AdditionResult.java
@@ -0,0 +1,14 @@
+package in.training;
+
+
+public class AdditionResult {
+
+public int addition() {
+int a=20;
+int b=10;
+int sum;
+	sum= a+b;
+	return sum;
+	
+}
+}
---
+++ ObsquraTraining/src/in/training/EligibilityTest.java
@@ -0,0 +1,11 @@
+package in.training;
+
+public class EligibilityTest {
+public static void main(String []args) {
+	int marks = 60;
+	if(marks>50) {
+		System.out.println("The candidate is eligible for admission");
+	}
+	
+}
+}
